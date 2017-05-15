410fe889a8c9150f6cd7de2a35c8b86e38185ab7
Alexander Juda <alexander.juda@icloud.com>
2017-05-15 23:00:15 +0200

    Add pngBytes() to Surface

--- Sources/Cairo/Surface.swift
+++ Sources/Cairo/Surface.swift
@@ -60,6 +60,66 @@ public final class Surface {
         cairo_surface_write_to_png(internalPointer, filepath)
     }
     
+    struct ByteBuffer {
+        var bytesPointer: UnsafeMutableRawPointer
+        var capacity: Int
+        var savedOffset: Int
+        
+        init(capacity: Int) {
+            self.capacity = capacity
+            self.bytesPointer = UnsafeMutableRawPointer.allocate(bytes: capacity,
+                                                                 alignedTo: MemoryLayout<UInt8>.alignment)
+            self.savedOffset = 0
+        }
+        
+        var savedBytes: [UInt8] {
+            var bytes: [UInt8] = []
+            for i in 0..<self.savedOffset {
+                let byte = self.bytesPointer.advanced(by: i).load(as: UInt8.self)
+                bytes.append(byte)
+            }
+            
+            return bytes
+        }
+    }
+    
+    public func pngBytes() -> [UInt8] {
+        
+        let width = Int(cairo_image_surface_get_width(internalPointer))
+        let height = Int(cairo_image_surface_get_height(internalPointer))
+        let bytesPerPixel = 4
+        
+        var bytesBuffer = ByteBuffer(capacity: width * height * bytesPerPixel)
+        
+        cairo_surface_write_to_png_stream(internalPointer, {
+            (context: UnsafeMutableRawPointer?,       // void *
+             dataChunkPointer: UnsafePointer<UInt8>?, // const uint8_t *
+             dataChunkLength: UInt32)
+             -> cairo_status_t in
+            
+            var bytesBuffer = context!.load(as: ByteBuffer.self)
+            
+            let bytesCount = Int(dataChunkLength)
+            for i in 0..<bytesCount {
+                let currentBytePointer = dataChunkPointer!.advanced(by: i)
+                let currentByte = currentBytePointer.pointee
+                
+                let savedOffset = bytesBuffer.savedOffset
+                bytesBuffer.bytesPointer
+                    .advanced(by: savedOffset+i)
+                    .storeBytes(of: currentByte, as: UInt8.self)
+            }
+            
+            bytesBuffer.savedOffset += bytesCount
+            
+            context!.copyBytes(from: &bytesBuffer, count: MemoryLayout<ByteBuffer>.size)
+            
+            return CAIRO_STATUS_SUCCESS
+        }, &bytesBuffer)
+        
+        return bytesBuffer.savedBytes
+    }
+    
     // MARK: - Accessors
     
     public var type: SurfaceType {
@@ -71,4 +131,11 @@ public final class Surface {
         
         return surfaceType
     }
+    
+    public var bytes: UnsafeMutablePointer<UInt8> {
+        return cairo_image_surface_get_data(internalPointer)!
+    }
+    
+    
 }
+
